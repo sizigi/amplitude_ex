58d0e296342147496e16e47b8e4b043ab621f954
Ben Yee <benyee@gmail.com>
2019-01-03 01:06:07 -0800

    add UUID dep

--- mix.exs
+++ mix.exs
@@ -28,6 +28,7 @@ defmodule Amplitude.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
+      {:elixir_uuid, "~> 1.2"},
       {:httpoison, "~> 1.5.0"},
       {:poison, "~> 3.1.0"} 
     ]
